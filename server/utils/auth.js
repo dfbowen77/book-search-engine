3c17327d2e0a58e7f585ded103583311642a5c2b
Dan Bowen <danbowen2677@gmail.com>
2023-06-20 11:06:58 -0400

    updated server.js file to implement apollo server and made comments in auth.js

--- server/utils/auth.js
+++ server/utils/auth.js
@@ -6,8 +6,10 @@ const expiration = '2h';
 
 module.exports = {
   // function for our authenticated routes
+  // CHANGED req, res, next to { req }
   authMiddleware: function ({ req }) {
     // allows token to be sent via  req.body, req.query, or headers
+    // ADDED req.body.token
     let token = req.body.token || req.query.token || req.headers.authorization;
 
     // ["Bearer", "<tokenvalue>"]
@@ -16,6 +18,7 @@ module.exports = {
     }
 
     if (!token) {
+      // CHANGED res.status... to req
       return req;
     }
 
@@ -25,9 +28,11 @@ module.exports = {
       req.user = data;
     } catch {
       console.log('Invalid token');
+      // REMOVED res.status...
     }
 
     // return the request object
+    // REMOVED next() and REPLACED it with req
     return req;
   },
   signToken: function ({ username, email, _id }) {
