b67bc2c313ee2174a38b35c41c776bcdf3be54f1
Dan Bowen <danbowen2677@gmail.com>
2023-06-21 12:21:19 -0400

    added mutations to the mutations.js file

---
+++ client/src/utils/mutations.js
@@ -0,0 +1,68 @@
+// Apollo Client is used to communnicate to the graphql server
+import { gql } from '@apollo/client';
+
+// executes the loginUser mutation
+export const LOGIN_USER = gql`
+    mutation loginUser($email: String!, $password: String!){
+        loginUser(email: $email, password: $password) {
+            token
+            user {
+                _id
+                username
+                email
+            }
+        }
+    }
+`
+
+// executes the addUser mutation
+export const ADD_USER = gql`
+    mutation addUser($username: String!, $email: String!, $password: String!){
+        addUser(username: $username, email: $email, password: $password){
+            token
+            user {
+                _id
+                username
+                email
+            }
+        }
+    } 
+`
+
+// executes the saveBook mutation
+export const SAVE_BOOK = gql`
+    mutation saveBook($input: BookInput!){
+        saveBook(input: $input){
+            _id
+            username
+            email
+            savedBooks {
+                bookId
+                authors
+                title
+                description
+                image
+                link
+            }
+        }
+    }
+`
+
+// executes the removeBook mutation
+export const REMOVE_BOOK = gql`
+    mutation removeBook($bookId: String!){
+        removeBook(bookId: $bookId){
+            _id
+            username
+            email
+            savedBooks {
+                bookId
+                authors
+                title
+                description
+                image
+                link
+            }
+        }
+    }
+`
--- client/src/utils/queries.js
+++ client/src/utils/queries.js
@@ -1,6 +1,7 @@
 // Apollo Client is used to communnicate to the graphql server
 import { gql } from '@apollo/client';
 
+// executes the 'me' query
 export const GET_ME = gql`
     query me {
         me {
