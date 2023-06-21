8b45d59318700f01c933c43051e60dffc75db45f
Dan Bowen <danbowen2677@gmail.com>
2023-06-21 11:51:02 -0400

    created mutations.js and queries.js; added code to queries.js

---
+++ client/src/utils/queries.js
@@ -0,0 +1,21 @@
+// Apollo Client is used to communnicate to the graphql server
+import { gql } from '@apollo/client';
+
+export const GET_ME = gql`
+    query me {
+        me {
+          _id
+          username
+          email
+          bookCount
+          savedBooks {
+            bookId
+            authors
+            title
+            description
+            image
+            link
+          }  
+        }
+    }
+`
